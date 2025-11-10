8aad9b538aa1ee01b99031fb82e353d3d5d86395
Swathi G <Swathisr9590@gmail.com>
2025-11-10 20:03:21 +0530

    58725

---
+++ orders/serializers.py
@@ -0,0 +1,8 @@
+from rest_framework import serializers
+from .models import Coupon
+
+
+class CouponSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Coupon
+        fields = ['code', 'discount_percentage', 'is_active', 'valid_from', 'valid_untill']
