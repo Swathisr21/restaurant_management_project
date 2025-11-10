970c590288e0c16f27c59b01b398d35c18de785d
Swathi G <Swathisr9590@gmail.com>
2025-11-10 20:24:57 +0530

    58723

--- orders/models.py
+++ orders/models.py
@@ -3,6 +3,13 @@ from django.db import models
 # Create your models here.
 from django.utils import timezone
 
+class OrderStatus(models.Model):
+    name = mpdels.CharField(max_length=50, unique=True)
+
+
+def __str__(self):
+    return self.name
+
 
 class Coupon(models.Model):
     code = models.CharField(max_length=50, unique=True)
@@ -14,3 +21,14 @@ class Coupon(models.Model):
 
     def __str__(self):
         return f"{self.code} ({self.discount_percentage}% off)"
+
+class Order(models.Model):
+    customer_name = models.CharField(max_length=100)
+    total_price = models.DecimalField(max_digits=10, decimal_places=2)
+    Created_at = models.DateTimeField(auto_now_add=True)
+
+    status = models.ForeignKey(OrderStatus, on_delete=models.SET_NULL, null=True)
+
+
+    def __str__(self):
+        return f"Order #{self.id} - {self.status.name if self.status else 'No Status'}"        
