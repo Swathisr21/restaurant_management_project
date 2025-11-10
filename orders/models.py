657e6a790dc4a27304a742a8b538ad27ce0b9f62
Swathi G <Swathisr9590@gmail.com>
2025-11-10 20:30:03 +0530

    58722

--- orders/models.py
+++ orders/models.py
@@ -3,6 +3,15 @@ from django.db import models
 # Create your models here.
 from django.utils import timezone
 
+# menu category  model
+class MenuCategory(models.model):
+    name = models.CharField(max_length=100, unique=True)
+
+
+    def __str__(self):
+        return self.name
+
+# order status
 class OrderStatus(models.Model):
     name = mpdels.CharField(max_length=50, unique=True)
 
@@ -10,7 +19,7 @@ class OrderStatus(models.Model):
 def __str__(self):
     return self.name
 
-
+# coupon
 class Coupon(models.Model):
     code = models.CharField(max_length=50, unique=True)
     discount_percentage = models.DecimalField(max_digits=5, decimal_places=2)
@@ -22,6 +31,7 @@ class Coupon(models.Model):
     def __str__(self):
         return f"{self.code} ({self.discount_percentage}% off)"
 
+# order 
 class Order(models.Model):
     customer_name = models.CharField(max_length=100)
     total_price = models.DecimalField(max_digits=10, decimal_places=2)
