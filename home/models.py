4d262bd79fd7607d171066fd012671f6280754c6
Swathi G <Swathisr9590@gmail.com>
2025-11-13 20:48:36 +0530

    60320

--- home/models.py
+++ home/models.py
@@ -4,6 +4,12 @@ from django.db import models
 class MenuCategory(model.Model):
     name = models.CharField(max_length=100)
 
+class MenuItem(models.Model):
+    name = models.CharField(max_length=100)
+    description = models.TextField()
+    price = models.DecimalField(max_digits=8, decimal_places=2)
+
+    is_featured = models.BooleanField(default=False)
 
     def __str__(self):
         return self.name
