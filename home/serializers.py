cbc7384a23f56d169a8ec25f3ad4a16dd3726e82
Swathi G <Swathisr9590@gmail.com>
2025-11-13 20:50:49 +0530

    60320

--- home/serializers.py
+++ home/serializers.py
@@ -1,8 +1,12 @@
 from rest_framework import serializers
 from .models import MenuCategory
-
+from .models import MenuItem
 
 class MenuCategorySerializer(serializers.ModelSerializer):
     class Meta:
         model = MenuCategory
         fields = ['id','name']
+
+class MenuItemSerializer(serializers.ModelSerializer):
+    class Meta = MenuItem
+    fields = '__all__'        
