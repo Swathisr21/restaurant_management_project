ade1f00605ec62a3b889ff6cc0c8990d168fd4e4
Swathi G <Swathisr9590@gmail.com>
2025-11-13 21:10:04 +0530

    60319

--- orders/serializers.py
+++ orders/serializers.py
@@ -6,3 +6,13 @@ class CouponSerializer(serializers.ModelSerializer):
     class Meta:
         model = Coupon
         fields = ['code', 'discount_percentage', 'is_active', 'valid_from', 'valid_untill']
+
+class OrderSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Order
+        fields = ['id', 'created_at', 'total_price', 'customer_name']
+
+class OrderItemSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = OrderItem
+        field = ['item_name', 'quantity','price']                
