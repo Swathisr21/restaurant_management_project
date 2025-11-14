7c20dbb2386000b6c05770b47780898c5b54c94c
Swathi G <Swathisr9590@gmail.com>
2025-11-14 19:54:47 +0530

    60571

--- home/models.py
+++ home/models.py
@@ -1,7 +1,7 @@
 from django.db import models
 
 # Create your models here.
-class MenuCategory(model.Model):
+class MenuCategory(models.Model):
     name = models.CharField(max_length=100)
 
 class MenuItem(models.Model):
@@ -13,3 +13,21 @@ class MenuItem(models.Model):
 
     def __str__(self):
         return self.name
+
+class DailySpecial(models.Model):
+    name = models.CharField(max_length=100)
+    description = models.TextField()
+    price = models.DecimalField(max_digits=8, decimal_places=2)
+    available = models.BooleanField(default=True)
+
+    @staticmethod
+    def get_random_special():
+        specials = DailySpecial.objects.filter(available=True)
+
+        if not specials.exists():
+            return None
+
+        return specials.order_by('?').first()
+
+    def __str__(self):
+        return self.name                
