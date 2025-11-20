1a6f8ec94f3f8197429fc3937e2969ae53d3b97a
Swathi G <Swathisr9590@gmail.com>
2025-11-20 21:16:06 +0530

    60864

--- home/models.py
+++ home/models.py
@@ -45,3 +45,22 @@ class NutritionalInformation(models.Model):
 
     def __str__(self):
         return f"Nutritional Info for {self.menu_item.name} - {self.calories} calories"                     
+class Ingredient(models.Model): 
+    name = models.CharField(max_length=100) 
+
+    def __str__(self):
+        return self.name
+
+
+# Update MenuItem model by adding ingredients field 
+Class MenuItem(models.Model)
+    name = models.CharField(max_length=100)
+    description = models.TextField() 
+    price = models.DecimalField(max_digits=8, decimal_places=2)
+    is_featured = models.BooleanField(default=False)
+
+    # New field
+    Ingredients = models.ManyToManyField(Ingredient, related_name='menu_items', blank=True)
+
+    def __str__(self):
+        return f"{self.name} - ₹{self.price}"          
