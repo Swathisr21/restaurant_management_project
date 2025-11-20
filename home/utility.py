2ca7fe8ef7b886b4745825cc6eaa024f464714ec
Swathi G <Swathisr9590@gmail.com>
2025-11-20 20:55:42 +0530

    60863

--- home/utility.py
+++ home/utility.py
@@ -1,4 +1,4 @@
-from datetime import datetime
+from datetime import datetime, time
 from .models import DailyOperatingHours
 
 def get_today_operating_hours():
@@ -15,3 +15,23 @@ def get_today_operating_hours():
     except DailyOperatingHours.DoesNotExist:
        # if no record found, restaurant might be closed
         return (None, None)   
+
+def is_restaurant_open():
+    # Get current day and time
+    now = datetime.now()
+    current_day = now.weekday()
+    current_time = now.time()
+
+    # Weekdays (mon-fri):9 AM - 10 PM
+    weekday_open = time(9, 0)
+    weekday_close = time(22, 0)
+
+    # Weekend (sat-sun):10 AM - 11 PM
+    weekend_open = time(10, 0)
+    weekend_close = time(23, 0)
+
+    # Check if restaurant is open
+    if current_day < 5:   # Monday-Friday
+        return weekday_open <= current_time <= weekday_close
+        else:   # Saturday-Sunday
+            return weekend_open <= current_time <= weekend_close
