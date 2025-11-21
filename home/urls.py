380256dbeec00f9d1fb6e5e8c14d0af6d631087d
Swathi G <Swathisr9590@gmail.com>
2025-11-21 22:10:26 +0530

    60865

--- home/urls.py
+++ home/urls.py
@@ -1,11 +1,11 @@
 from django.urls import path
-from .views import MenuCategoryListview, FeaturedMenuItemsView, MenuItemSearchViewset, MenuItemIngredientsView
+from .views import (MenuCategoryListview, FeaturedMenuItemsView, MenuItemSearchViewset, MenuItemIngredientsView)
 
 menu_item_search = MenuItemSearchViewset.as_view({'get': 'list'})
 
 urlpatterns = [
-    path('menu-categories/',MenuCategoryListview.as_view(),name='menu_categories'),
-    path('featured-items/', FeaturedMenuItemsView.as_view(), name='featured_menu_item'),
-    path("menu/search/", menu_item_search, name="menu_item_search"), 
-    path("menu-item/<int:pk>/ingredients/", MenuItemIngredientsView.as_view(), name="menu_item_ingredients"),
+    path('menu/categories/',MenuCategoryListview.as_view(),name='menu_categories'),
+    path('menu/featured-items/', FeaturedMenuItemsView.as_view(), name='featured_menu_item'),
+    path("menu/items/search/", menu_item_search, name="menu_item_search"), 
+    path("menu/items/<int:pk>/ingredients/", MenuItemIngredientsView.as_view(), name="menu_item_ingredients"),
 ]
