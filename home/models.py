f924d668269d61f1c44ad72c3e0835e00d27de6a
Swathi G <Swathisr9590@gmail.com>
2025-11-21 22:38:21 +0530

    60866

--- home/models.py
+++ home/models.py
@@ -5,30 +5,26 @@ import datetime
 class MenuCategory(models.Model):
     name = models.CharField(max_length=100)
 
-class MenuItem(models.Model):
-    name = models.CharField(max_length=100)
-    description = models.TextField()
-    price = models.DecimalField(max_digits=8, decimal_places=2)
-
-    is_featured = models.BooleanField(default=False)
-
     def __str__(self):
         return f"{self.name} - ₹{self.price}"
 class DailySpecialManager(models.Manager):
     def upcoming(self):
         today = datetime.date.today()
-        return super().get_quertset().filter(date__gte=today, available=True)
+        return super().get_queryset().filter(date__gte=today, available=True)
         
 class DailySpecial(models.Model):
     name = models.CharField(max_length=100)
     description = models.TextField()
     price = models.DecimalField(max_digits=8, decimal_places=2)
     available = models.BooleanField(default=True)
+    date = models.DateField()
+
+    objects = DailySpecialManager()
+
 
     @staticmethod
     def get_random_special():
         specials = DailySpecial.objects.filter(available=True)
-
         if not specials.exists():
             return None
 
@@ -50,6 +46,7 @@ class NutritionalInformation(models.Model):
 
     def __str__(self):
         return f"Nutritional Info for {self.menu_item.name} - {self.calories} calories"                     
+
 class Ingredient(models.Model): 
     name = models.CharField(max_length=100) 
 
@@ -58,14 +55,14 @@ class Ingredient(models.Model):
 
 
 # Update MenuItem model by adding ingredients field 
-Class MenuItem(models.Model)
+class MenuItem(models.Model)
     name = models.CharField(max_length=100)
     description = models.TextField() 
     price = models.DecimalField(max_digits=8, decimal_places=2)
     is_featured = models.BooleanField(default=False)
 
     # New field
-    Ingredients = models.ManyToManyField(Ingredient, related_name='menu_items', blank=True)
+    ingredients = models.ManyToManyField(Ingredient, related_name='menu_items', blank=True)
 
     def __str__(self):
         return f"{self.name} - ₹{self.price}"          
