48b7604e44a68639a92e96a88a4b6c7edba70b05
Swathi G <Swathisr9590@gmail.com>
2025-11-21 22:58:27 +0530

    60867

--- home/urls.py
+++ home/urls.py
@@ -3,15 +3,20 @@ from .views import (
     MenuCategoryListview, 
     FeaturedMenuItemsView, 
     MenuItemSearchViewset, 
-    MenuItemIngredientsView
+    MenuItemIngredientsView,
+    MenuItemViewSet
     )
 
 # for viewSet List endpoint
 menu_item_search = MenuItemSearchViewset.as_view({'get': 'list'})
+menu_item_update = MenuItemViewSet.as_view({
+    'put': 'update',
+})
 
 urlpatterns = [
     path('menu/categories/',MenuCategoryListview.as_view(),name='menu_categories'),
     path('menu/featured-items/', FeaturedMenuItemsView.as_view(), name='featured_menu_item'),
     path("menu/items/search/", menu_item_search, name="menu_item_search"), 
     path("menu/items/<int:pk>/ingredients/", MenuItemIngredientsView.as_view(), name="menu_item_ingredients"),
+    path("menu-items/<int:pk>/update", menu_item_update, name="menu_item_update"),
 ]
