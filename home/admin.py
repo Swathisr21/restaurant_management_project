877ad1a620610d90f3e49eb67091c94c5ee783fe
Swathi G <Swathisr9590@gmail.com>
2025-11-29 20:49:40 +0530

    61159

--- home/admin.py
+++ home/admin.py
@@ -1,3 +1,19 @@
 from django.contrib import admin
+from .models import Restaurant
+
 
 # Register your models here.
+
+class RestaurantAdmin(admin.ModelAdmin):
+
+# 3. Show these fields in List view
+List-display = ('name', 'address', 'phone_number', 'email', 'is_active')
+
+# 4. Enable search by name or address
+search_fields = ('name', 'address')
+
+# 5. Filter by active/inactive if the has an is_active field
+list_filter = ('is_active',)
+
+# 6. Register Restaurant model with the admin
+admin.site.register(Restaurant, RestaurantAdmin)
