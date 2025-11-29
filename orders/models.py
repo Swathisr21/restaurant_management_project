2f773ab6f72f6879797222d8d1afa1835fd475c4
Swathi G <Swathisr9590@gmail.com>
2025-11-29 19:51:21 +0530

    61157

--- orders/models.py
+++ orders/models.py
@@ -52,6 +52,7 @@ class Order(models.Model):
         ('cancelled', 'Cancelled'),
     )
     
+    user = models.ForeignKey(User, on_delete=models.CASCADE, related_name="orders")
     customer_name = models.CharField(max_length=100)
     total_price = models.DecimalField(max_digits=10, decimal_places=2)
     Created_at = models.DateTimeField(auto_now_add=True)
