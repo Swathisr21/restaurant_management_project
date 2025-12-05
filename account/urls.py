b601a2f7bb398d2ed9c797a991bbe22073ace0d5
Swathi G <Swathisr9590@gmail.com>
2025-12-05 21:04:07 +0530

    61444

--- account/urls.py
+++ account/urls.py
@@ -1,6 +1,7 @@
 from django.urls import path
-from .views import *
+from .views import UserProfileViewSet
 
 urlpatterns = [
+    path('profile/update/', UserProfileViewSet.as_view({'put': 'updated'}), name= 'profile-update'),
     
 ]
