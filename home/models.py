40109dd207ab3f2456b51fbf04ba1ef0995a43b2
Swathi G <Swathisr9590@gmail.com>
2025-12-08 19:06:09 +0530

    61447

--- home/models.py
+++ home/models.py
@@ -101,3 +101,23 @@ class Restaurant(models.Model):
 
     def __str__(self):
         return self.name
+
+class LoyaltyProgram(models.Model):
+    name = models.CharField(
+        max_length=50,
+        unique=True
+    )
+
+    points_Required = models.IntegerField(
+        unique=True
+    )  
+
+    discount_percentage = models.DecimalField(
+        max_digits=5,
+        decimal_places=2
+    )
+
+    description = models.TextField()
+
+    def __str__(self):
+        return self.name
