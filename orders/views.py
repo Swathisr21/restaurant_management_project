a660bafd210fc77e4393969b658c7b59df52c5a1
Swathi G <Swathisr9590@gmail.com>
2026-01-09 14:31:42 +0530

    62735

--- orders/views.py
+++ orders/views.py
@@ -5,6 +5,7 @@ from rest_framework.views import APIView
 from rest_framework.response import Response
 from rest_framework.permissions import IsAuthenticated
 from .models import Order
+from .serializers import OrderSerializer
 
 class OrderHistoryView(APIView):
     permission_classes = [IsAuthenticated]
