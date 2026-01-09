def3555c361b29efd37ec030ce9790f2f2f53d0e
Swathi G <Swathisr9590@gmail.com>
2026-01-09 15:01:43 +0530

    62735

--- orders/serializers.py
+++ orders/serializers.py
@@ -13,18 +13,33 @@ class OrderStatusSerializer(serializers.ModelSerializer):
         fields = ['id', 'name']
 
 class OrderItemSerializer(serializers.ModelSerializer):
+    item_name = serializers.CharField(source='menu_item.name', read_only=True)
+    price = serializers.DecimalField(
+        source='Menu_item.price',
+        max_digits=8,
+        decimal_places=2,
+        read_only=True
+    )
+
     class Meta:
         model = OrderItem
         fields = ['item_name', 'quantity','price']                
 
 class OrderSerializer(serializers.ModelSerializer):
     status = OrderStatusSerializer(read_only=True)
+    items = OrderItemSerializer(
+        source='orderitem_set',
+        many=True,
+        read_only=True
+    )
 
     class Meta:
         model Order
         fields = [
             'id',
+            'order_id',
             'customer_name',
             'Created_at',
             'status',
+            'items',
         ]        
