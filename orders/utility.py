47c23493c46b9de96a97ceb6f5a470c59becd038
Swathi G <Swathisr9590@gmail.com>
2026-01-09 14:42:57 +0530

    62735

--- orders/utility.py
+++ orders/utility.py
@@ -7,6 +7,7 @@ from decimal import Decimal
 import logging
 from django.core.mail import send_mail
 from django.conf import settings
+from 
 from smtplib import SMTPException
 
 logger = logging.getlogger(__name__)
