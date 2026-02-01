eab4520d1b3d74b05fa11289c17127833e1c7325
Swathi G <Swathisr9590@gmail.com>
2026-02-01 12:37:30 +0530

    66078

--- orders/serializers.py
+++ orders/serializers.py
@@ -8,10 +8,30 @@ class CouponSerializer(serializers.ModelSerializer):
         fields = ['code', 'discount_percentage', 'is_active', 'valid_from', 'valid_until']
 
 class OrderStatusSerializer(serializers.ModelSerializer):
+    """
+    Serializer to update order status
+    """
+    order_id = serializers.CharField()
+    status = serializers.CharField()
     class Meta:
         model = OrderStatus
         fields = ['id', 'name']
 
+    def validate_status(self, value):
+        allowed_statuses = ["pending", "processing", "completed"]    
+        
+        if value.lower() not in allowed_statuses:
+            raise serializers.ValidationError(
+                f"Invalid status. Allowed values: {allowed_statuses}"
+            )
+        return value.lower()
+
+    def validate_order_id(self, value):
+        if not Order.objects.filter(order_id=value).exists():
+            raise serializers.ValidationError("Order with this ID does not exist.")
+        return value
+
+                   
 class OrderItemSerializer(serializers.ModelSerializer):
     item_name = serializers.CharField(source='menu_item.name', read_only=True)
     price = serializers.DecimalField(
