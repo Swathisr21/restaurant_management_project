dcaf06808a1d5b41a4fad7f1d88af0e683f3dc22
Swathi G <Swathisr9590@gmail.com>
2026-02-02 22:42:54 +0530

    66106

--- orders/urls.py
+++ orders/urls.py
@@ -1,5 +1,5 @@
 from django.urls import path
-from .views import CouponValidationView, OrderStatusUpdateView
+from .views import CouponValidationView, OrderStatusUpdateView, get_order_status
 from .views import OrderHistoryView, PaymentMethodListView, CancelOrderView, UpdateOrderStatusView
 urlpatterns = [
     
@@ -9,4 +9,5 @@ urlpatterns = [
     path('orders/update-status/', OrderStatusUpdateView.as_view(), name='order-update-status'),
     path('orders/cancel/', CancelOrderView.as_view(), name="cancel-order"),
     path('orders/update-status/', UpdateOrderStatusView.as_view(), name='update-order-status'),
+    path('order/status/<str:order_id>/', get_order_status, name='order-status'),
 ]
