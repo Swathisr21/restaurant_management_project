776ad8314523baab5d569aadd86870c84eb5ce32
Swathi G <Swathisr9590@gmail.com>
2026-02-02 21:30:17 +0530

    66103

--- home/urls.py
+++ home/urls.py
@@ -7,7 +7,8 @@ from .views import (
     MenuItemViewSet,
     TableDetailView,
     AvailableTablesAPIView,
-    MenuItemViewSet
+    MenuItemViewSet,
+    MenuItemReviewViewSet
     )
 from rest_framework.routers import DefaultRouter
 
@@ -25,14 +26,17 @@ menu_item_update = MenuItemViewSet.as_view({
 })
 
 urlpatterns = [
-    path('menu/categories/',MenuCategoryListview.as_view(),name='menu_categories'),
+    path('menu/categories/', MenuCategoryListview.as_view(),name='menu_categories'),
     path('menu/featured-items/', FeaturedMenuItemsView.as_view(), name='featured_menu_item'),
     path("menu/items/search/", menu_item_search, name="menu_item_search"), 
     path("menu/items/<int:pk>/ingredients/", MenuItemIngredientsView.as_view(), name="menu_item_ingredients"),
     path("menu-items/<int:pk>/update", menu_item_update, name="menu_item_update"),
     path("tables/<int:pk>/", TableDetailView.as_view(), name="table_detail"),
     path('api/tables/available/', AvailableTablesAPIView.as_view(), name='available_tables_api'),
-    path('daily-specials/',DailySpecialsView.as_view(), name="daily-specials"),
+    path('daily-specials/', DailySpecialsView.as_view(), name="daily-specials"),
     path("", include(router.urls)),
+    path("reviews/create/", CreateUserReviewView.as_view()),
+    path("menu-items/<int:menu_item_id>/reviews/", MenuItemReviewViewSet.as_view(), name="menu-item-reviews"),
+
 ]
 
