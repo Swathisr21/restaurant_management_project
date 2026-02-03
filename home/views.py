cee013b8262c8e94b6b15a9f212fe4d67e9e45fc
Swathi G <Swathisr9590@gmail.com>
2026-02-03 20:08:24 +0530

    66131

--- home/views.py
+++ home/views.py
@@ -1,9 +1,40 @@
 from rest_framework.views import APIView
 from rest_framework.response import response
 from rest_framework import status
-from .models import Restaurant
-from .serializers import RestaurantSerializer
+from .models import Restaurant, MenuItem
+from .serializers import RestaurantSerializer, MenuItemAvailabilitySerializer
 
+class UpdateMenuItemAvailabilityView(APIView):
+    """
+    Update menu item availability
+    """
+
+    def put(self, request, menu_item_id):
+        try:
+            menu_item = MenuItem.objects.get(id=menu_item_id)
+        except MenuItem.DoesNotExist:
+            return Response(
+                {"error": "Menu item not found"},
+                status=status.HTTP_404_NOT_FOUND
+            )
+
+        serializer = MenuItemAvailabilitySerializer(data=request.data)
+
+        if serializer.is_valid():
+            menu_item.is_available = serializer.validated_data["is_available"]
+            menu_item.save()
+
+            return Response(
+                {
+                    "message": "Menu item availability updated successfully",
+                    "menu_item_id": menu_item.id,
+                    "is_available": menu_item.is_available,
+                },
+                status=status.HTTP_200_OK
+            )        
+
+        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+            
 class RestaurantInfoView(APIView):
     """
     GET restaurant details
